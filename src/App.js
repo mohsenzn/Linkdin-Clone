f0670f41a6d38d2e5f30a107ee61a7588246c281
mohsen.zergani <mohsen.zergani@gmail.com>
2021-07-21 21:19:25 +0430

    sidebar

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
 import React from "react";
 import "./App.css";
 import Header from "./Components/Header/Header";
+import Sidebar from "./Components/Sidebar/Sidebar";
 
 function App() {
   return (
     <div className="App">
       <Header />
 
-      {/* app body */}
-      {/* sidebar */}
-      {/* feed */}
-      {/* widgets */}
+      <div className="app__body">
+        <Sidebar />
+        {/* feed */}
+        {/* widgets */}
+      </div>
     </div>
   );
 }
---
+++ src/Components/Sidebar/Sidebar.js
@@ -0,0 +1,46 @@
+import React from "react";
+import "./Sidebar.css";
+import { Avatar } from "@material-ui/core";
+
+const Sidebar = () => {
+  const recentItem = (topic) => (
+    <div className="sidebar__recentItem">
+      <span className="sidebar__hash"># </span>
+      <p> {topic}</p>
+    </div>
+  );
+  return (
+    <div className="sidebar">
+      <div className="sidebar__top">
+        <img
+          src="https://images.unsplash.com/photo-1542831371-29b0f74f9713?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=750&q=80"
+          alt=""
+        />
+        <Avatar className="sidebar__avatar">{}</Avatar>
+        <h2>mohsen zergani</h2>
+        <h4>test@gmail.com</h4>
+      </div>
+      <div className="sidebar__stats">
+        <div className="sidebar_stat">
+          <p>Who viewed you</p>
+          <p className="sidebar__statNumber">2,543</p>
+        </div>
+        <div className="sidebar_stat">
+          <p>Views on post</p>
+          <p className="sidebar__statNumber">2,448</p>
+        </div>
+      </div>
+      <div className="sidebar__bottom">
+        <p>Recent</p>
+        {recentItem("React js")}
+        {recentItem("Programming")}
+        {recentItem("Software engineering")}
+        {recentItem("Design")}
+        {recentItem("Developer")}
+        {recentItem("React js")}
+      </div>
+    </div>
+  );
+};
+
+export default Sidebar;

6361860c31f9133ee459bb97425244cc7f9509fe
mohsen.zergani <mohsen.zergani@gmail.com>
2021-07-24 23:32:27 +0430

    Login

--- src/App.js
+++ src/App.js
@@ -3,16 +3,24 @@ import "./App.css";
 import Header from "./Components/Header/Header";
 import Sidebar from "./Components/Sidebar/Sidebar";
 import Feed from "./Components/Feed/Feed";
+import { useDispatch, useSelector } from "react-redux";
+import { selectUser } from "./features/Userslice";
+import Login from "./Components/Login/Login";
 function App() {
+  const user = useSelector(selectUser);
+  const dispatch = useDispatch();
   return (
     <div className="App">
       <Header />
-
-      <div className="app__body">
-        <Sidebar />
-        <Feed />
-        {/* widgets */}
-      </div>
+      {!user ? (
+        <Login />
+      ) : (
+        <div className="app__body">
+          <Sidebar />
+          <Feed />
+          {/* widgets */}
+        </div>
+      )}
     </div>
   );
 }
